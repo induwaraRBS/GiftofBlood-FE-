a2330cdf4d9ba79dbce55bfeab3a083e28c72e0c
induwara <induwarakanchana567@gmail.com>
2023-03-25 18:02:48 +0530

    home button done

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -2,8 +2,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react'
 import "./Banner.css";
 import { Button } from 'reactstrap';
+import { useNavigate } from 'react-router-dom';
+
 
 function Banner() {
+  const navigate = useNavigate();
+
+  const bsignup = () =>{
+    navigate('/signup');
+    console.log("button1");
+  }
+
+  const blogin = () => {
+    navigate('/login');
+    console.log("button2");
+  }
   return (
     <><div className='banner'>
       <img src='https://images.unsplash.com/photo-1615461065624-21b562ee5566?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Ymxvb2QlMjBkb25hdGlvbnxlbnwwfHwwfHw%3D&w=1000&q=80' alt='' />
@@ -14,11 +27,11 @@ function Banner() {
         <p>lorem abdjanw ajnwdjnawdm ajunwdnaiwdanwdjnanadwlnadiald <br />
           ajnwdnalwdnlaidwl djnwawlndilanwdila ajnwldnalwndilawndli</p>
 
-          <Button className='banner_button1' color="danger"  size="lg">
+          <Button onClick ={bsignup} className='banner_button1' color="danger"  size="lg">
               Register
               </Button>
 
-           <Button className='banner_button2' color="danger"   size="lg">
+           <Button onClick={blogin} className='banner_button2' color="danger"   size="lg">
               Login
               </Button>    
 
